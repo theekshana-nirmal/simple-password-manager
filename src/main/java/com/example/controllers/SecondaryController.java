eaf3bcd3f9853204b0531a9ebdadbea41af8e649
Theekshana Nirmal <theekshanab1234@gmail.com>
2025-08-02 12:13:46 +0530

    Refactor FXML file paths and add primary and secondary view layouts

--- src/main/java/com/example/controllers/SecondaryController.java
+++ src/main/java/com/example/controllers/SecondaryController.java
@@ -9,6 +9,6 @@ public class SecondaryController {
 
     @FXML
     private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
+        App.setRoot("fxml/primary");
     }
 }
