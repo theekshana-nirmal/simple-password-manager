3c0ff9c5e8825ce41490c1c9710c8e60f8d910b0
Theekshana Nirmal <theekshanab1234@gmail.com>
2025-08-05 12:44:59 +0530

    Change password field type from PasswordField to TextField in AddPasswordController and FXML

--- src/main/java/com/example/controllers/AddPasswordController.java
+++ src/main/java/com/example/controllers/AddPasswordController.java
@@ -4,7 +4,6 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
@@ -23,7 +22,7 @@ public class AddPasswordController implements Initializable {
     private TextField usernameField;
 
     @FXML
-    private PasswordField passwordField;
+    private TextField passwordField;
 
     @FXML
     private Button saveButton;
